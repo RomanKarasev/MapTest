c0cf10557e436031ad0da99d95fb7e39d3a39d5e
Roman <mail@karasevroman.ru>
2022-04-07 12:41:49 +0300

    Initial Commit

---
+++ MapTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapTest
+//
+//  Created by Роман Карасёв on 07.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
